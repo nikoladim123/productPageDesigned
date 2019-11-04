42bb5e560d1155c0a0831c55ac02dc53a008ec81
unknown <nikoladimitrijevic1991@gmail.com>
2019-11-04 21:07:27 +0100

    megamenu added all

--- assets/js/productPage.js
+++ assets/js/productPage.js
@@ -25,7 +25,59 @@ navItem[0].addEventListener('mouseleave',()=>{
   cosmeticsMM[0].style.maxHeight = '0vh';
 });
 
+// eyesMM
+var eyesMM = document.getElementsByClassName('eyesMM');
 
+navItem[1].addEventListener('mouseenter',()=>{
+  eyesMM[0].style.maxHeight = '70vh';
+});
+
+navItem[1].addEventListener('mouseleave',()=>{
+  eyesMM[0].style.maxHeight = '0vh';
+});
+
+
+// hair mm
+var hairMM = document.getElementsByClassName('hairMM');
+var hairMMImage = document.getElementsByClassName('hairMMImage');
+var hairMMitem = document.getElementsByClassName('hairMMitem');
+
+navItem[2].addEventListener('mouseenter',()=>{
+  hairMM[0].style.maxHeight = '70vh';
+});
+
+navItem[2].addEventListener('mouseleave',()=>{
+  hairMM[0].style.maxHeight = '0vh';
+});
+
+
+hairMMitem[0].addEventListener('mouseover',()=>{
+  hairMMImage[0].src = 'assets/images/productPage/images/cosmetics/secTwo/3.png';
+});
+
+hairMMitem[1].addEventListener('mouseover',()=>{
+  hairMMImage[0].src = 'assets/images/productPage/images/cosmetics/secTwo/1.png';
+});
+
+hairMMitem[2].addEventListener('mouseover',()=>{
+  hairMMImage[0].src = 'assets/images/productPage/images/cosmetics/secTwo/2.png';
+});
+
+hairMMitem[3].addEventListener('mouseover',()=>{
+  hairMMImage[0].src = 'assets/images/productPage/images/cosmetics/secTwo/3.png';
+});
+
+hairMMitem[4].addEventListener('mouseover',()=>{
+  hairMMImage[0].src = 'assets/images/productPage/images/cosmetics/secTwo/1.png';
+});
+
+hairMMitem[5].addEventListener('mouseover',()=>{
+  hairMMImage[0].src = 'assets/images/productPage/images/cosmetics/secTwo/2.png';
+});
+
+hairMMitem[6].addEventListener('mouseover',()=>{
+  hairMMImage[0].src = 'assets/images/productPage/images/cosmetics/secTwo/3.png';
+});
 
 
 window.addEventListener('scroll',()=>{
